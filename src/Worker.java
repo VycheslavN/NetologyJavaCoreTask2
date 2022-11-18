4363e566c043db3e2a53970816fadd8d2dc3809b
Vyacheslav Nechepurenko <masterdog8@mail.ru>
2022-11-18 20:50:46 +0300

    initial commit

---
+++ src/Worker.java
@@ -0,0 +1,37 @@
+public class Worker {
+
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    private OnTaskDoneListener callback;
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    private OnTaskErrorListener errorCallback;
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            if (i == 33) {
+                errorCallback.onError("TASK " + i + " IS NOT DONE!");
+            }else callback.onDone("Task " + i + " is done");
+        }
+    }
+
+    public static void main(String[] args) {
+        OnTaskDoneListener listener = System.out::println;
+        OnTaskErrorListener errorListener = System.out::println;
+        Worker worker = new Worker(listener, errorListener);
+        worker.start();
+
+    }
+}
